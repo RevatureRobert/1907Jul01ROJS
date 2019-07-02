1a9c063d3343d8e1383ad98f18c345194c0ae081
connellrobert <robert.lel.connell@gmail.com>
2019-07-02 16:48:26 -0400

    css and the intro to js

---
+++ js/hellojs.js
@@ -0,0 +1,79 @@
+/* 
+I'm sure you have heard of java
+    Javascript has nothing to do with java
+
+    A few things
+        it is a programming language
+        it is a scripting language
+        it is interpreted
+            the browser will run js line by line
+        It is loosely typed
+        it is dynamic
+*/
+
+/* 
+    js is loosely typed because variables can hold 
+        and have transferred any datatype
+    In js:
+        x="fitty"
+    in java:
+        String x="fitty"
+    in js:
+        x=6;
+    in java:
+        NO X=6
+*/
+
+
+// x="sum sum"
+/* 
+    datatypes in js
+        string, boolean, number, object, symbol, undefined, null
+    different types of dattypes
+        primitive
+            number, string, boolean, null, undefined, symbol
+        referenced
+            object, function, array
+*/
+////////////////////// what is a variable
+// variables are placeholders that we can place our values in
+
+////////////////// how do we declare a variable
+// var, let, const, no_keyword
+// var y=1;
+// let z=2;
+// const c=4;
+// a=2;
+
+///////////////operators
+//  3 types of operators:
+//      unary, binary, ternary
+//  unary works with one operand
+//  binary works with two operand
+//  ternary works with three operand
+/* 
+unary
+    !   returns the opposite boolean value
+    ++x pre increment
+    x++ post increment
+    --x pre decrement
+    x-- post decrement
+
+binary
+    +   addition
+    -   subtraction
+    *   multiplication
+    /   division
+    **  exponent
+    %   modulus
+    <   less than
+    >   greater than
+    <=  less than or equal to
+    >=  greater than or equal to
+    =   assignment
+    x+=2  addition assignment
+    -=  subtraction assignment
+    *=  multiplication assignment
+    /=  division assignment
+
+*/
